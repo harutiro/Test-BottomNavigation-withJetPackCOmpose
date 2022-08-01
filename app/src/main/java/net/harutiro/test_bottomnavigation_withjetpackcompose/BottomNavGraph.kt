98397eb4947a14ae9cd53ac7db6fbb8d53cb3193
harutiro <harutiro2027@outlook.jp>
2022-08-01 11:42:32 +0900

    update:ファイル名の変更

--- app/src/main/java/net/harutiro/test_bottomnavigation_withjetpackcompose/BottomNavGraph.kt
+++ app/src/main/java/net/harutiro/test_bottomnavigation_withjetpackcompose/BottomNavGraph.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import net.harutiro.test_bottomnavigation_withjetpackcompose.screens.HomeScreen
 
 @Composable
 fun BottomBavGraph(navController:NavHostController) {
